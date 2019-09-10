5aad1af8e3d7a71f9db2d548466c3204e941d893
Vannida Lim <vannida731@gmail.com>
2019-09-10 10:32:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   index = 0
   while index < array.length do 
+    puts 'ARRAY[Index]' + array[index]
     squared = array[index] * array[index]
+    puts 'SQUARED' + squared
     array[index] = squared
     index += 1
   end
